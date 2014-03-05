2684d4ed4ce09064c472ae7f577003d6a597ec78
Lorenzo Delgado Antequera <rafellorens@gmail.com>
2014-03-05 16:56:33 +0100

    Solved soundEffects mutting.

--- OneButtonLarry/src/org/scbio/onebuttonlarry/stage/GapJump.java
+++ OneButtonLarry/src/org/scbio/onebuttonlarry/stage/GapJump.java
@@ -18,24 +18,14 @@ public class GapJump extends GameStage {
 	 */
 	private static final int BG_RES = R.drawable.stagebg_gapjump;
 	private static final float Y_GROUND = 0.626f;
-
-	private Context context;
-	private GameView parent;
-
-	private JumpLarry larry;
 	
-	SoundPool soundPool;
-	int soundJump;
-	int soundDead;
+	private GameView parent;
+	private JumpLarry larry;
 
 	public GapJump(Context context, GameView parent) {	
-		this.context = context;
 		this.parent = parent;
 
 		larry = new JumpLarry(context, parent, 0.1f);
-		soundPool = new SoundPool( 5, AudioManager.STREAM_MUSIC , 0);
-		soundJump = soundPool.load(context, R.raw.jumpsound, 0);
-		soundDead = soundPool.load(context, R.raw.deadsound, 0);
 		
 		larry.setPos(-larry.getWidth()/2, Y_GROUND*parent.getHeight());
 		larry.setIncX(JumpLarry.LARRY_REGSPEED);
@@ -72,18 +62,17 @@ public class GapJump extends GameStage {
 		if(larry.isJumping()){ 
 			larry.setIncY(larry.jump(delay, larry.getIncY()));
 			
-			if(larry.getIncY() >= -(5+JumpLarry.LARRY_JUMPSPEEDY))
+			if(larry.getIncY() >= -(2+JumpLarry.LARRY_JUMPSPEEDY))
 			{
 				larry.jump = false;
 				larry.setIncY(0);
 				larry.setIncX(JumpLarry.LARRY_REGSPEED);
-				larry.setPosY(parent.getHeight()*Y_GROUND-1);
+				larry.setPosY(parent.getHeight()*Y_GROUND);
 			}
 		}
 		
-		if(!larry.isJumping() && larry.hasFallen()){
+		if(larry.hasFallen()){
 			larry.setPos(-larry.getWidth()/2, Y_GROUND*parent.getHeight());
-			soundPool.play(soundDead, 1, 1, 1, 0, 1);
 		}
 		
 		larry.incPos(delay);
@@ -91,21 +80,6 @@ public class GapJump extends GameStage {
 	}
 	
 	private class JumpLarry extends Larry{
-		public JumpLarry(Context context, View view) {
-			super(context, view);
-		}
-		
-		public boolean hasFallen() {
-			boolean firstgap = (getPosX() > 0.11*parent.getWidth() && getPosX() < 0.18*parent.getWidth());
-			boolean secondgap = (getPosX() > 0.26*parent.getWidth() && getPosX() < 0.43*parent.getWidth()); 
-			boolean thirdgap = (getPosX() > 0.52*parent.getWidth() && getPosX() < 0.68*parent.getWidth());
-			boolean fourthgap = (getPosX() > 0.76*parent.getWidth() && getPosX() < 0.81*parent.getWidth());
-			return firstgap || secondgap || thirdgap || fourthgap;
-		}
-		
-		public JumpLarry(Context context, View view, float scale) {
-			super(context, view, scale);
-		}
 		
 		/*
 		 * Larry constants.
@@ -115,27 +89,52 @@ public class GapJump extends GameStage {
 		public static final float LARRY_JUMPSPEEDX = 0.01171875f;
 		public static final float GRAVITY = 5f;
 		
+		private SoundPool mSoundPool;
+		private int jumpSound;
+		private int deathSound;
+		
 		private boolean jump = false;
+		
+		public JumpLarry(Context context, View view) {
+			super(context, view);
+			
+			mSoundPool = new SoundPool(2, AudioManager.STREAM_MUSIC , 0);
+			jumpSound = mSoundPool.load(context, LARRY_SOUND_JUMP, 1);
+			deathSound = mSoundPool.load(context, LARRY_SOUND_DIE, 1);
+		}		
+		public JumpLarry(Context context, View view, float scale) {
+			super(context, view, scale);
+			mSoundPool = new SoundPool(2, AudioManager.STREAM_MUSIC , 0);
+			jumpSound = mSoundPool.load(context, LARRY_SOUND_JUMP, 1);
+			deathSound = mSoundPool.load(context, LARRY_SOUND_DIE, 1);
+		}
+		
+		@Override
+		protected void doAction() {
+			jump = true;
+			if(parent.areGameSoundEffectsEnabled()) mSoundPool.play(jumpSound, 1, 1, 1, 0, 1);
+			setIncY(LARRY_JUMPSPEEDY);
+			setIncX(LARRY_JUMPSPEEDX*parent.getWidth());
+		}
+		
+		public double jump(double t, double vinit) {
+			return vinit + GRAVITY*t;
+		}
 		
 		public boolean isJumping(){
 			return jump;
 		}
 		
-		/*
-		 * Larry jump action 
-		 * @see org.scbio.onebuttonlarry.game.Larry#doAction()
-		 */
-		@Override
-		protected void doAction() {
-			jump = true;
-			soundPool.play(soundJump, 1, 1, 1, 0, 1);
-			setIncY(LARRY_JUMPSPEEDY);
-			setIncX(LARRY_JUMPSPEEDX*parent.getWidth());
-		}
-		
-		public double jump(double t, double vinit)
+		public boolean hasFallen()
 		{
-			return vinit + GRAVITY*t;
+			boolean firstgap = (getPosX() > 0.11*parent.getWidth() && getPosX() < 0.18*parent.getWidth());
+			boolean secondgap = (getPosX() > 0.26*parent.getWidth() && getPosX() < 0.43*parent.getWidth()); 
+			boolean thirdgap = (getPosX() > 0.52*parent.getWidth() && getPosX() < 0.68*parent.getWidth());
+			boolean fourthgap = (getPosX() > 0.76*parent.getWidth() && getPosX() < 0.81*parent.getWidth());
+			boolean result = (firstgap || secondgap || thirdgap || fourthgap) && jump;
+			
+			if(result && parent.areGameSoundEffectsEnabled()) mSoundPool.play(deathSound, 1, 1, 1, 0, 1);
+			return result;
 		}
 	}
 }
